95f00e562045dee074ab418bfdb7a673274556be
Ayoub Umoru <ayoub.umoru@zerodotfive.com>
2020-02-21 12:36:51 +0100

    high priv monitor

--- scripts/crontab.sh
+++ scripts/crontab.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo (crontab -l 2>/dev/null; echo "*/1 * * * * /home/pi/raspi-box/tools/monitorMetrics.sh >/tmp/cronny.log 2>&1") | crontab -
+(sudo crontab -l 2>/dev/null; echo "*/1 * * * * /home/pi/raspi-box/tools/monitorMetrics.sh >/tmp/cronny.log 2>&1") | sudo crontab
